44262ff1b3359b0088f4945cd9adf7e646038028
mdferdausalam19 <mdferdausalam333@gmail.com>
2024-12-23 17:18:14 +0600

    fix: Implement Content Security Policy with helmet middleware to resolve CSP violations

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kmxsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -21,12 +22,12 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const database = client.db("exploreAsia");
     const touristSpots = database.collection("touristSpots");
 
+    // routes
     app.get("/tourist-spots", async (req, res) => {
       const cursor = touristSpots.find();
       const result = await cursor.toArray();
@@ -96,14 +97,9 @@ async function run() {
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    console.log("Connected to MongoDB successfully!");
+  } catch (err) {
+    console.error("Error connecting to MongoDB: ", err.message);
   }
 }
 run().catch(console.dir);
@@ -115,7 +111,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(
-    `ExploreAsia server is up and running! Listening on port ${port} for incoming connections.`
-  );
+  console.log(`ExploreAsia server is running on port ${port}`);
 });
